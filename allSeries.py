50fb43ff4cd8c572be0979750a1a7747252a078e
erstert3st <lura12111@gmail.com>
2022-11-22 18:14:33 +0100

    deleted:    season.py

--- allSeries.py
+++ allSeries.py
@@ -2,10 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 
 #should only run once 
-def main(self):
+def Main(self):
      Crawl_Page()
 
-def Crawl_Page(self):
+def Crawl_Page():
     URL = 'https://bs.to/andere-serien' # may chech over random 
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
@@ -24,3 +24,33 @@ def Crawl_Page(self):
         print(single.text)
         #To DB
     print(len(dataA))
+
+
+def Crawl_Episode():
+     URL = 'https://bs.to/serie/Die-Simpsons-The-Simpsons/2/de' # may chech over random 
+     page = requests.get(URL)
+     soup = BeautifulSoup(page.content, "html.parser")
+     # get season Data
+     dataDiv = soup.find("div", {"id": "seasons"})
+     dataDiv = dataDiv.find_all("li")
+     dataDiv[-1]
+     print(dataDiv[0])
+     print(dataDiv[-1])
+          #ToDo DB
+def Crawl_Seasons(self):
+     URL = 'https://bs.to/serie/Die-Simpsons-The-Simpsons' # may chech over random 
+     page = requests.get(URL)
+     soup = BeautifulSoup(page.content, "html.parser")
+
+     # get season Data
+     dataDiv = soup.find("div", {"id": "seasons"})
+     dataDiv = dataDiv.find_all("li")
+     dataDiv[-1]
+     print(dataDiv[0])
+     print(dataDiv[-1])
+          #ToDo DB
+
+#pip install mysql-connector-python
+#sudo apt-get install libmariadb3 libmariadb-dev
+if __name__ == "__main__":
+    Main()
