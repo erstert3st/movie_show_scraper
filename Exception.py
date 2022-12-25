66a524056a105479ba1d7aa3d0b6c6b4bae47a8a
kill <erstert3st@gmail.com>
2022-12-25 15:44:09 +0100

    zwichencommit streamkiste done exce video handler

--- Exception.py
+++ Exception.py
@@ -3,7 +3,13 @@ class videoBroken(Exception):
         return "File is not online"
 class botDetection(Exception):
     def __str__(self):
-        return "File is not online"
+        return "bot is found"
+class streamKisteSearchError(Exception):
+    def __str__(self):
+        return "bot is found"
+class invalidImdb(Exception):
+    def __str__(self):
+        return "bot is found"    
 class captchaLock(Exception):
     def __str__(self):
         return "To many request try again later"
